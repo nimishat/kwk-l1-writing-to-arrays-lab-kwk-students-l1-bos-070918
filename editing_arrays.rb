fda02e9e8bfef0af91b997f2a456851b155e74e1
nimishat <nimishathakkar83@gmail.com>
2018-07-11 14:46:48 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -6,7 +6,8 @@ RAINBOW_COLORS = [
 
 def change_rainbow_colors
   RAINBOW_COLORS[0] = red
-  RAINBOW_COLORS[]
+  RAINBOW_COLORS[1] = light_red
+  RAINBOW_COLORS[2] = light_yellow
 end
 
 def add_colors
